22914c56b3a25815e1894d6b958df06259b2595a
Nathan <natefintak@gmail.com>
2025-03-22 17:26:28 -0400

    start of SortByPriceTest class

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,23 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] items;
+
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        List<Item> itemsToSort = Arrays.asList(this.items);
+        itemsToSort.sort(comparator);
+        Item[] itemsSorted = itemsToSort.toArray(new Item[0]);
+        return itemsSorted;
     }
 }
