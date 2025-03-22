ac4ca2f11c24d2b8146855df9b9da9b72346895d
Nathan <natefintak@gmail.com>
2025-03-22 16:46:41 -0400

    NameComparator class/tests complete

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -8,5 +8,7 @@ import java.util.Comparator;
  * @author leon on 30/01/2019.
  */
 public class NameComparator implements Comparator<Item> {
-    public int compare();
+    public int compare(Item one, Item two) {
+        return one.getName().compareTo(two.getName());
+    }
 }
