2bc3d4f7c53e9838a5445abf2cb26936650f7886
Nathan <natefintak@gmail.com>
2025-03-22 17:50:44 -0400

    outline for SortByPriceTest class

--- src/test/java/rocks/zipcode/io/itemsorter/SortByPriceTest.java
+++ src/test/java/rocks/zipcode/io/itemsorter/SortByPriceTest.java
@@ -1,7 +1,65 @@
 package rocks.zipcode.io.itemsorter;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcode.io.Item;
+import rocks.zipcode.io.ItemSorter;
+import rocks.zipcode.io.comparators.PriceComparator;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
 public class SortByPriceTest {
+    //should only change price var for each here
+    @Test
+    public void test1() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void test2() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void test3() {
+        //given
+
+        //when
+
+        //then
+
+    }
+    //check what happens when 2 items have the same price?
+    @Test
+    public void test4() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void test5() {
+        //given
+
+        //when
+
+        //then
+
+    }
 }
