ac7c9400b804410eb369756fddbea7690c5df9cd
Nathan <natefintak@gmail.com>
2025-03-22 16:48:56 -0400

    start on ItemSorter class

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,13 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+    public int compare(Item one, Item two) {
+        return one.getPrice().compareTo(two.getPrice());
+    }
 }
